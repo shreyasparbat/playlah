cd63c163ccbf2268cd64461fd004d63e3b77afa4
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-10-16 20:46:00 +0800

    added cors

--- api/app.js
+++ api/app.js
@@ -22,8 +22,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/client', clientRouter);
+app.use('/client', clientRouter)
 app.use('/scores', scoresRouter)
+
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
