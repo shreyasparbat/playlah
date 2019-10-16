9ca1bf5fabe83238c60b245a7e4edab87419cbad
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-10-16 19:30:16 +0800

    routes working

--- api/routes/client.js
+++ api/routes/client.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const router = express.Router()
-const { pool } = require('../../db')
+const { pool } = require('../db')
 
 /* GET all clients */
 router.get('/all', async (req, res) => {
   try {
       const poolRes = await pool.query('select distinct clientname from demo')
+      res.send(poolRes.rows)
+  } catch (error) {
+      console.log(error.stack)
+      res.status(500).send({
+          error: error.stack
+      })
+  }
+})
+
+/* POST get scores of a client (demo game) */
+router.post('/demo-game-scores', async (req, res) => {
+  try {
+      const poolRes = await pool.query('select tappedkey, timestamp from demo where clientname = $1', [req.body.clientname])
       res.send(poolRes.rows)
   } catch (error) {
       console.log(error.stack)
--- api/routes/scores.js
+++ api/routes/scores.js
@@ -1,26 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { pool } = require('../../db')
-
-/* POST scores of a client (demo game) */
-router.post('/demo-game', async (req, res) => {
-  try {
-      const poolRes = await pool.query('select tappedkey, timestamp from demo where clientname = $1', [req.name])
-      res.send(poolRes.rows)
-  } catch (error) {
-      console.log(error.stack)
-      res.status(500).send({
-          error: error.stack
-      })
-  }
-})
+const { pool } = require('../db')
 
 /* POST add scores in demo game for a given client */
 router.post('/add-demo-scores', async (req, res) => {
   try {
     let qString = 'insert into demo (clientname, tappedkey, timestamp) values '
     const qParams = []
-    req.scores.forEach(score => {
+    req.body.scores.forEach(score => {
       qParams.push(score.clientname)
       qString += '($' + qParams.length
       qParams.push(score.tappedkey)
@@ -28,7 +15,7 @@ router.post('/add-demo-scores', async (req, res) => {
       qParams.push(score.timestamp)
       qString += ',$' + qParams.length + '), '
     })
-    await pool.query(qString.substring(0, qString.length-2))
+    await pool.query(qString.substring(0, qString.length-2), qParams)
     res.send('success')
 } catch (error) {
     console.log(error.stack)
